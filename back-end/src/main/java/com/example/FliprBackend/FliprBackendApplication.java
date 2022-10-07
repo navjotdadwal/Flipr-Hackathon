6e385986c9d4e832f92444846c42663dc0fe7ad4
navjotdadwal <navjotsinghdadwal@gmail.com>
2022-10-07 19:35:15 +0530

    initial setup

---
+++ back-end/src/main/java/com/example/FliprBackend/FliprBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FliprBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliprBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FliprBackendApplication.class, args);
+	}
+
+}
